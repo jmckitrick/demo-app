2d59ca3e6d6841d434518d18adabc91613dbd7c2
Jonathon McKitrick <jonathon.mckitrick@tstllc.net>
2018-03-28 06:04:02 -0400

    Add designers query

--- src/clj/demo_app/routes/services.clj
+++ src/clj/demo_app/routes/services.clj
@@ -76,15 +76,37 @@
   (let [{:keys [id]} args]
     (get games-map id)))
 
+(defn resolve-board-game-designers
+  [designers-map context args board-game]
+  (->> board-game
+       :designers
+       ;; map an id-based map of designers (as a fn)
+       ;; over a set of designer ids
+       (map designers-map)))
+
+(defn resolve-designer-games
+  [games-map context args designer]
+  (let [{:keys [id]} designer]
+    (->> games-map
+         vals
+         (filter #(-> % :designers (contains? id))))))
+
+(defn entity-map
+  [data k]
+  (reduce #(assoc %1 (:id %2) %2)
+          {}
+          (get data k)))
+
 (defn resolver-map
   []
   (let [cgg-data (-> (io/resource "graphql/cgg-data.edn")
                      slurp
                      edn/read-string)
-        games-map (->> cgg-data
-                       :games
-                       (reduce #(assoc %1 (:id %2) %2) {}))]
-    {:query/game-by-id (partial resolve-game-by-id games-map)}))
+        games-map (entity-map cgg-data :games)
+        designers-map (entity-map cgg-data :designers)]
+    {:query/game-by-id (partial resolve-game-by-id games-map)
+     :BoardGame/designers (partial resolve-board-game-designers designers-map)
+     :Designer/games (partial resolve-designer-games games-map)}))
 
 (defstate compiled-schema
   :start
